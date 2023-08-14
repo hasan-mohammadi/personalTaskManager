3d4405b7b718a81b8aa17a36051e0e64b41bb8d9
Hasan <h4347255@gmail.com>
2023-08-14 19:18:23 +0330

    setup splash screen

--- app/src/main/java/com/example/personaltaskmanager/MainActivity.kt
+++ app/src/main/java/com/example/personaltaskmanager/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.personaltaskmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
